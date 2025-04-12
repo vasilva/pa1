fd748602198e8fbe15259475b7e858edcf113cfb
vasilva <vinicius.alex89@gmail.com>
2025-04-11 23:38:29 -0300

    Refactor Crawler class to enhance functionality and improve error handling
    - Added support for robots.txt parsing using Protego to respect crawling rules.
    - Implemented methods to download and check robots.txt for allowed URLs.
    - Enhanced error handling for HTTP requests with detailed logging in debug mode.
    - Introduced a JSON format output for page content extraction.
    - Improved URL management by using sets for visited and to-visit URLs.
    - Added command-line argument parsing for seed URLs and crawling limits in main.py.

--- crawler.py
+++ crawler.py
@@ -1,17 +1,38 @@
 import logging
+import time
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+from protego import Protego
 import requests
 from requests.compat import urljoin
 
 
 logging.basicConfig(
-    # filename="crawler.log",
+    filename="crawler.log",
     filemode="w",
-    format="%(asctime)s,%(msecs)03d\n    %(levelname)s: %(message)s",
+    format="%(asctime)s,%(msecs)03d\n" + "%(levelname)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     level=logging.INFO,
 )
+
+json_format = {
+    "URL": "",
+    "Title": "",
+    "Text": "",
+    "Timestamp": 0,
+}
+
+# Constants
+# Maximum number of words to extract from the page
+MAX_TEXT_SIZE = 20
+# Maximum time to wait for a response
+TIMEOUT = 5  # in seconds
+# Maximum time to wait between requests
+WAIT_TIME = 100  # in milliseconds
+
+
+class CrawlerException(Exception):
+    """Exception for the Crawler class."""
 
 
 class Crawler:
@@ -19,20 +40,109 @@ class Crawler:
     A simple web crawler that visits a list of URLs and extracts links from them.
     """
 
-    def __init__(self, urls=[], max_urls=100):
+    def __init__(
+        self,
+        urls: set[str] = set(),
+        max_urls: int = 100,
+        max_depth: int = 20,
+        debug: bool = False,
+    ):
         """
         Initialize the crawler with a list of URLs to visit.
 
         Parameters
         ----------
-            urls (list[str]): A list of URLs to start crawling from.
+            urls (set[str]): A list of URLs to start crawling from.
+            max_depth (int): The maximum depth to crawl.
             max_urls (int): The maximum number of URLs to visit.
+            debug (bool): If True, enable debug mode.
         """
-        self.urls_visited = set()
         self.urls_to_visit = urls
+        self.urls_visited = set()
+        self.urls_disallowed = set()
+        self.robots_txt = {}
         self.max_urls = max_urls
+        self.max_depth = max_depth
+        self.current_depth = 0
+        self.debug = debug
 
-    def download_url(self, url):
+    def get_base_url(self, url: str):
+        """
+        Extract the base URL from a given URL.
+
+        Parameters
+        ----------
+            url (str): The URL to extract the base URL from.
+        Returns
+        -------
+            str: The base URL.
+        """
+        if not url.startswith("http"):
+            return ""
+        split_url = url.split("/")
+        base_url = split_url[0] + "//" + split_url[2]
+        return base_url
+
+    def get_robots_txt(self, url: str):
+        """
+        Get the robots.txt file for a given URL.
+
+        Parameters
+        ----------
+            url (str): The URL to get the robots.txt file for.
+        Returns
+        -------
+            str: The content of the robots.txt file.
+        """
+        base_url = self.get_base_url(url)
+        if base_url not in self.robots_txt:
+            robots_txt = self.download_robots_txt(url)
+            self.robots_txt[base_url] = robots_txt
+
+        return self.robots_txt[base_url]
+
+    def download_robots_txt(self, url: str):
+        """
+        Download the robots.txt file from the given URL.
+
+        Parameters
+        ----------
+            url (str): The URL to download the robots.txt file from.
+        Returns
+        -------
+            str: The content of the robots.txt file.
+        """
+        base_url = self.get_base_url(url)
+        if not base_url:
+            if self.debug:
+                logging.exception(f"Invalid URL: {url}")
+            return ""
+        
+        # Download the robots.txt file
+        try:
+            robots_txt = requests.get(urljoin(base_url, "/robots.txt"), timeout=TIMEOUT)
+        
+        except requests.exceptions.RequestException:
+            if self.debug:
+                logging.exception(f"Failed to download robots.txt: {url}")
+            return ""
+        
+        except requests.exceptions.HTTPError:
+            if self.debug:
+                logging.exception(f"Failed to download robots.txt: {url}")
+            return ""
+        
+        # Check if the response is successful
+        if robots_txt.status_code == 200:
+            if self.debug:
+                logging.info(f"Downloaded robots.txt: {base_url}")
+            return robots_txt.text
+        else:
+            if self.debug:
+                logging.exception(f"Failed to download robots.txt: {base_url}")
+            return ""
+
+    def download_url(self, url: str):
         """
         Download the content of a URL.
 
@@ -43,18 +153,99 @@ class Crawler:
         -------
             str: The HTML content of the URL.
         """
-        response = requests.get(url)
-        if response.status_code != 200:
-            logging.exception(f"Failed to download: {url}")
-            return None
+        try:
+            response = requests.get(url, timeout=TIMEOUT)
 
-        logging.info(f"Downloaded: {url}")
-        with open(f"out/{len(self.urls_visited)}.txt", "w") as f:
-            f.write(response.text)
-        # Return the HTML content
+        except requests.exceptions.RequestException:
+            if self.debug:
+                logging.exception(f"Failed to download: {url}")
+            return ""
+        
+        except requests.exceptions.HTTPError:
+            if self.debug:
+                logging.exception(f"Failed to download: {url}")
+            return ""
+
+        # Check if the response is successful
+        if response.status_code != 200:
+            if self.debug:
+                logging.exception(f"Failed to download: {url}")
+            return ""
+
+        if self.debug:
+            logging.info(f"Downloaded: {url}")
+            with open(f"out/{len(self.urls_visited)}.txt", "w") as f:
+                f.write(response.text)
+
         return response.text
 
-    def get_linked_urls(self, url, html):
+    def is_url_allowed(self, url: str):
+        """
+        Check if a URL is allowed to be crawled based on the robots.txt file.
+
+        Parameters
+        ----------
+            url (str): The URL to check.
+        Returns
+        -------
+            bool: True if the URL is allowed, False otherwise.
+        """
+        if url in self.urls_disallowed:
+            return False
+
+        robots_txt = self.get_robots_txt(url)
+        if not robots_txt:
+            return True
+        # Parse the robots.txt file
+        rp = Protego.parse(robots_txt)
+        # Check if the URL is allowed
+        allowed = rp.can_fetch(url, "*")
+        if not allowed:
+            if self.debug:
+                logging.info(f"URL disallowed by robots.txt: {url}")
+            self.urls_disallowed.add(url)
+        return allowed
+
+    def print_json_text(self, url: str, soup: BeautifulSoup):
+        """
+        Print the JSON format of the page content.
+        This function is used for debugging purposes.
+
+        Parameters
+        ----------
+            url (str): The URL of the page.
+            soup (BeautifulSoup): The BeautifulSoup object containing the HTML content.
+
+        Example
+        -------
+        The JSON format will look like this:
+
+        {
+            "URL": "http://example.com",
+            "Title": "Example Title",
+            "Text": "The first 20 words of the page.",
+            "Timestamp": 1234567890
+        }
+        """
+        # Extract text from the page
+        timestamp = int(time.time())
+        title_tag = soup.find("title")
+        title = title_tag.text if title_tag else ""
+
+        text_tag = soup.get_text()
+        text = text_tag.split() if text_tag else []
+        # Limit the text to the first 20 words
+        text = " ".join(text[:MAX_TEXT_SIZE]) if text else ""
+        # Create a JSON-like format
+        json_format["URL"] = url
+        json_format["Title"] = title
+        json_format["Text"] = text
+        json_format["Timestamp"] = timestamp
+
+        # Print the JSON format
+        print(json_format, end="\n\n")
+
+    def get_linked_urls(self, url: str, html: str):
         """
         Extract all linked URLs from the HTML content of a page.
 
@@ -69,11 +260,16 @@ class Crawler:
         soup = BeautifulSoup(html, "html.parser")
         for link in soup.find_all("a"):
             href = link.get("href")
-            if href and href.startswith("/"):
+            # Skip if the link is not a valid URL
+            if not href or not href.startswith(("http", "/")):
+                continue
+            # Convert relative URLs to absolute URLs
+            if href.startswith("/"):
                 href = urljoin(url, href)
+
             yield href
 
-    def add_urls_to_visit(self, url):
+    def add_urls_to_visit(self, url: str):
         """
         Add a URL to the list of URLs to visit if it hasn't been visited yet.
 
@@ -81,10 +277,15 @@ class Crawler:
         ----------
             url (str): The URL to add.
         """
-        if url not in self.urls_visited and url not in self.urls_to_visit:
-            self.urls_to_visit.append(url)
+        # Check if the URL is allowed to be crawled
+        if not self.is_url_allowed(url):
+            return
 
-    def crawl(self, url):
+        # Check if the URL is already visited or disallowed
+        if url not in self.urls_visited and url not in self.urls_disallowed:
+            self.urls_to_visit.add(url)
+
+    def crawl(self, url: str):
         """
         Crawl a URL, download its content, and extract linked URLs.
 
@@ -92,7 +293,35 @@ class Crawler:
         ----------
             url (str): The URL to crawl.
         """
+        self.get_robots_txt(url)
+        # Check if the URL is allowed to be crawled
+        if not self.is_url_allowed(url):
+            if self.debug:
+                logging.info(f"URL disallowed: {url}")
+            return
+
+        # Check if the maximum depth is reached
+        self.current_depth += 1
+        if self.debug:
+            logging.info(f"Current depth: {self.current_depth}")
+        if self.current_depth > self.max_depth:
+            if self.debug:
+                logging.info(f"Maximum depth reached: {url}")
+            self.current_depth = 0
+            return
+
+        # Download the content of the URL
         html = self.download_url(url)
+
+        # Check if the HTML content is valid
+        if not html:
+            return
+
+        if self.debug:
+            soup = BeautifulSoup(html, "html.parser")
+            self.print_json_text(url, soup)
+
+        # Extract linked URLs and add them to the list of URLs to visit
         for url in self.get_linked_urls(url, html):
             self.add_urls_to_visit(url)
 
@@ -100,25 +329,27 @@ class Crawler:
         """
         Start the crawling process.
         """
+        previous_url = ""
         while self.urls_to_visit and len(self.urls_visited) < self.max_urls:
-            url = self.urls_to_visit.pop(0)
-            logging.info(f"Crawling: {url}")
+            url = self.urls_to_visit.pop()
+            domain = self.get_base_url(url)
+            if domain == self.get_base_url(previous_url):
+                time.sleep(WAIT_TIME / 1000)
+            
+            if self.debug:
+                logging.info(f"Crawling: {url}")
+            
             try:
                 self.crawl(url)
-            except Exception:
-                logging.exception(f"Failed to crawl: {url}")
+
+            except CrawlerException:
+                if self.debug:
+                    logging.exception(f"Failed to crawl: {url}")
+
             finally:
                 self.urls_visited.add(url)
+                previous_url = url
 
-
-crawler = Crawler(
-    urls=[
-        "http://www.r7.com/",
-        "http://mdemulher.abril.com.br/",
-        "http://www.personare.com.br/",
-    ],
-    max_urls=20,
-)
-
-crawler.run()
-print(len(crawler.urls_visited))
+        print(f"Visited {len(self.urls_visited)} URLs.")
+        print(f"Disallowed {len(self.urls_disallowed)} URLs.")
+        print(f"Remaining {len(self.urls_to_visit)} URLs.")
--- main.py
+++ main.py
@@ -1,12 +1,67 @@
-import requests
-import json
 import argparse
-from bs4 import BeautifulSoup
 from crawler import Crawler
 
+seeds_urls = [
+    "http://www.r7.com/",
+    "http://mdemulher.abril.com.br/",
+    "http://www.personare.com.br/",
+]
 
-# if __name__ == "__main__":
-#     crawler = Crawler(start_urls=["https://www.r7.com/"], max_pages=10)
-#     crawler.crawl()
+LIMIT = 5
 
-print(("http://www.r7.com/").split("/"))
+
+def get_args():
+    """
+    Parse command-line arguments.
+
+    Returns
+    -------
+        argparse.Namespace: Parsed command-line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="Web Crawler",
+        usage="%(prog)s -s <SEEDS> -n <LIMIT> [-d]",
+    )
+    parser.add_argument(
+        "-s",
+        "--Seeds",
+        help="File containing seed URLs",
+        type=str,
+        default="seeds-2017023617.txt",
+    )
+    parser.add_argument(
+        "-n",
+        "--Limit",
+        type=int,
+        help="Maximum number of URLs to visit",
+        default=LIMIT,
+    )
+    parser.add_argument(
+        "-d",
+        "--Debug",
+        action="store_true",
+        help="Enable debug mode",
+    )
+    return parser.parse_args()
+
+
+def main():
+    """
+    Main function to run the web crawler.
+    """
+    args = get_args()
+    seeds = args.Seeds
+    with open(seeds, "r") as f:
+        seeds = f.readlines()
+        seeds = {url.strip() for url in seeds}
+
+    crawler = Crawler(
+        urls=seeds,
+        max_urls=args.Limit,
+        debug=args.Debug,
+    )
+    crawler.run()
+
+
+if __name__ == "__main__":
+    main()
